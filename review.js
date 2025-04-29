ae3f14aebf29cb7e402a372694ab5034b62a90c8
ashirwadam <ashirwadam.rai@se.com>
2025-04-29 11:50:18 +0530

    mistakes

--- review.js
+++ review.js
@@ -4,16 +4,17 @@ function addNumbers(a, b, c) {
 }
 
 var myArray = [1, 2, 3, 4, 5];
-for (let i = 0; i < myArray.length; i++) { // Declared 'i' with 'let' to limit its scope
+for (var i = 0; i < myArray.length; i++) { // Declared 'i' with 'let' to limit its scope
   console.log(myArray[i]);
 }
 
-if (true) {
+if (false) {
   let x = 10; // Changed 'var' to 'let' to limit scope to the block
   console.log(x); // Moved 'console.log' inside the block
 }
 
 function doSomething() {
+  return;
   console.log("Doing something"); // Removed unnecessary return and dead code
 }
 
